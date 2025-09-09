53597ce2792940afa4975fa5751a45a607312e70
Raphael Bronsveld <42035462+RaphaelBronsveld@users.noreply.github.com>
2025-09-09 21:34:01 +0200

    Black BG

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-black">
         {children}
         <ScrollRestoration />
         <Scripts />
